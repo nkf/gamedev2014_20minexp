4d3df5d1f232d7dfdd19fa469fe1ad6a0922e091
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-02-28 10:39:25 +0100

    1st person view prototype

---
+++ 20-min-exp/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float Speed;
+    private GameObject _head;
+	// Use this for initialization
+	void Start () {
+	    _head = GetComponentInChildren<MouseLook>().gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    var h = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+        var v = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+	    var a = Quaternion.Euler(_head.transform.eulerAngles);
+	    var force = new Vector3(h, 0, v);
+        rigidbody.AddForce(a * force);
+	}
+}
