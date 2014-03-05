60ca72c168e3573bf1cd711d15a86bcd43159cb2
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-03-05 15:06:55 +0100

    Level loading framework with sample scenes

---
+++ 20-min-exp/Assets/Scripts/LevelController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(GUIText))]
+public class LevelController : MonoBehaviour {
+
+    private GUIText _text;
+	// Use this for initialization
+	void Start () {
+	    _text = GetComponent<GUIText>();
+        _text.pixelOffset = new Vector2(Screen.width/2, Screen.height/2);
+	    _text.enabled = false;
+	}
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private int _index = 1;
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyUp(KeyCode.F7)) {
+	        if (LevelLoader.Load("level" + _index)) {
+                StartCoroutine(LoadingText());
+	            _index--;
+	        }
+	    } else if (Input.GetKeyUp(KeyCode.F8)) {
+	        if (LevelLoader.Load("level" + _index)) {
+                StartCoroutine(LoadingText());
+	            _index++;
+	        }
+	    } else if (Input.GetKeyUp(KeyCode.F9)) {
+	        _text.enabled = false;
+	        LevelLoader.Switch();
+	    }
+	}
+
+    IEnumerator LoadingText() {
+        if (LevelLoader.Status == LoadStatus.NotLoading) yield break;
+        _text.enabled = true;
+        while (LevelLoader.Status == LoadStatus.Loading) {
+            _text.text = LevelLoader.Progress+"%";
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForEndOfFrame();
+        _text.text = 100 + "%";
+    }
+}
---
+++ 20-min-exp/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using UnityEngine;
+using System.Collections;
+using AsyncOperation = UnityEngine.AsyncOperation;
+
+public class LevelLoader {
+
+    private static AsyncOperation _currentLoadJob;
+    /// <summary>
+    /// Returns the current progess or -1 if there is no level loading.
+    /// </summary>
+    public static float Progress {
+        get { return _currentLoadJob == null ? -1 : Mathf.Round(_currentLoadJob.progress * 100f); }
+    }
+
+    public static LoadStatus Status {
+        get {
+            return _currentLoadJob == null
+                ? LoadStatus.NotLoading
+                : _currentLoadJob.progress >= 0.9f ? LoadStatus.Done : LoadStatus.Loading; 
+                //soo unity is fucked, loading will only go to 90% when allowSceneActivation is set to false and isDone will not trigger.
+        }
+    }
+
+    public static bool Load(string level) {
+        if (_currentLoadJob != null) {Debug.Log("Could not load \""+level+"\", because a level is already loading"); return false;}
+        _currentLoadJob = Application.LoadLevelAsync(level);
+        if(_currentLoadJob != null) _currentLoadJob.allowSceneActivation = false;
+        return _currentLoadJob != null;
+    }
+
+    public static void Switch() {
+        if (_currentLoadJob != null) {
+            _currentLoadJob.allowSceneActivation = true;
+            _currentLoadJob = null;
+        } else {
+            Debug.Log("Could not switch because no scene was loaded.");
+        }
+        
+    }
+}
+
+public enum LoadStatus {
+    Done, Loading, NotLoading
+}
