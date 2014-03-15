9173d7d9714ec3ff35362708a346ecdea8725d3d
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-03-15 17:37:29 +0100

    forgot to save scene.... gg

---
+++ 20-min-exp/Assets/Scripts/DinnerAnimationFunctions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DinnerAnimationFunctions : MonoBehaviour {
+
+    public GameObject Door1;
+    public ConversationAgent Conversation1;
+    public Camera Camera1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void StartConversation1() {
+        Conversation1.StartConversation();
+    }
+
+    public void OpenDoor1() {
+        var ani = Door1.GetComponent<Animation>();
+        ani.Play();
+    }
+
+    public void SwitchToCamera1() {
+        Camera1.SwitchTo();
+        Camera1.GetComponent<Animator>().Play("GoDownHall1");
+    }
+}
---
+++ 20-min-exp/Assets/Scripts/HallAnimationFunctions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HallAnimationFunctions : MonoBehaviour {
+
+    public ConversationAgent Conversation1;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    public void StartConversation1() {
+        Conversation1.StartConversation();
+    }
+
+}
