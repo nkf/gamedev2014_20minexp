9bc532b87c6cf36130f90ae9318e927c8df3e226
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-03-15 15:23:04 +0100

    Camera transitions

---
+++ 20-min-exp/Assets/Scripts/AnimationFunctions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationFunctions : MonoBehaviour {
+
+    public GameObject Door1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public void OpenDoor1() {
+        var ani = Door1.GetComponent<Animation>();
+        ani.Play();
+    }
+}
