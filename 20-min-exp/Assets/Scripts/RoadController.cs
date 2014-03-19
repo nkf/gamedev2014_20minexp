e5e07bd041f82e58518b050d0da0e0eb6d93cc3b
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-03-19 13:18:04 +0100

    Road minigame prototype

---
+++ 20-min-exp/Assets/Scripts/RoadController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class RoadController : MonoBehaviour {
+    
+    private GameObject _roadBelow;
+    private Bounds _roadBounds;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    public float Speed = 50;
+    public float SteeringSpeed = 0.5f;
+    public float SwayFactor = 0.02f;
+    public float EdgeSlowDownThreshold = 3f;
+    public float EdgePadding = 1f;
+    private float _slowDownFactor = 1;
+	// Update is called once per frame
+	void Update () {
+	    var p = transform.position;
+	    var hInput = Input.GetAxis("Horizontal");
+	    var move = hInput * SteeringSpeed + (Random.value*SwayFactor);
+	    //Calculate edge slowdown
+	    var edgeDis = DistanceToEdge();
+	    _slowDownFactor = Mathf.InverseLerp(0, EdgeSlowDownThreshold, edgeDis);
+        //if we are going too close to the edge AND are on course towards the edge.
+	    if (edgeDis < EdgePadding && Mathf.Sign(transform.position.x) == Mathf.Sign(move)) move = 0;
+        //apply
+	    p.x += move * _slowDownFactor;
+        p.z += Speed * Time.deltaTime * _slowDownFactor;
+	    transform.position = p;
+	    
+	}
+
+    void FixedUpdate() {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, Vector3.down, out hit)) {
+            var road = hit.collider.gameObject;
+            if (road != _roadBelow) EnteredRoad(road);
+            _roadBelow = road;
+            _roadBounds = _roadBelow.GetComponent<BoxCollider>().bounds;
+        }
+    }
+
+    //!!!!! THIS ONLY WORKS BECAUSE ROAD IS CENTERED @ x = 0 !!!!
+    private float DistanceToEdge() {
+         return Mathf.Abs(Mathf.Abs(transform.position.x) - _roadBounds.extents.x);
+    }
+
+    public event Action<GameObject> EnteredRoad;
+    protected virtual void OnEnteredRoad(GameObject obj) {
+        Action<GameObject> handler = EnteredRoad;
+        if (handler != null) handler(obj);
+    }
+}
---
+++ 20-min-exp/Assets/Scripts/RoadGenerator.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using Random = System.Random;
+
+
+public class RoadGenerator : MonoBehaviour {
+
+    public int RoadBufferSize;
+
+
+    private string[] _normalRoadSections;
+    private string[] _exoticRoadSections;
+    private Random _rng;
+    private const int RNGSeed = 100;
+    private readonly LinkedList<GameObject> _roadList = new LinkedList<GameObject>(); 
+	void Start () {
+	    var path = Application.dataPath + "/Resources/RoadPrefabs";
+	    var roadPaths = Directory.GetFiles(path).Select(s => CleanPath(s)).Distinct();
+	    _normalRoadSections = roadPaths.Where(s => s.Contains("[n]")).ToArray();
+	    _exoticRoadSections = roadPaths.Where(s => !s.Contains("[n]")).ToArray();
+        _rng = new Random(RNGSeed);
+	    for (int i = 0; i < RoadBufferSize; i++) {
+	        SpawnRoad();
+	    }
+	    Camera.main.GetComponent<RoadController>().EnteredRoad += road => {
+	        var firstRoad = _roadList.First.Value;
+	        if (road != firstRoad) {
+                _roadList.RemoveFirst();
+                Destroy(firstRoad);
+                SpawnRoad();
+	        }
+	    };
+	}
+
+    private float _z;
+    private int _normalRoadCounter = 0;
+    private int _normalRoadAmount = 5;
+
+    private void SpawnRoad() {
+        if(_normalRoadCounter == _normalRoadAmount) {
+            _normalRoadCounter = 0;
+            _normalRoadAmount = _rng.Next(3, 10);
+            _z += CreateRoadTile(new Vector3(0, 0, _z), RoadType.Exotic);
+        } else {
+            _normalRoadCounter++;
+            _z += CreateRoadTile(new Vector3(0, 0, _z), RoadType.Normal);
+        }
+    }
+
+    private string getExoticSection() {
+        return _exoticRoadSections[ _rng.Next(0, _exoticRoadSections.Length) ];
+    }
+    private string getNormalSection() {
+        return _normalRoadSections[ _rng.Next(0, _normalRoadSections.Length) ];
+    }
+    private enum RoadType { Normal, Exotic }
+    private string getSection(RoadType type) {
+        if (type == RoadType.Normal) return getNormalSection();
+        if (type == RoadType.Exotic) return getExoticSection();
+        return null;
+    }
+
+    private int roadID = 0;
+    private float CreateRoadTile(Vector3 pos, RoadType type) {
+        var load = Resources.Load<GameObject>(getSection(type));
+        var road = (GameObject)Instantiate(load);
+        road.transform.position = pos;
+        road.transform.parent = transform;
+        road.name = "#"+(roadID++)+" road ("+load.name+")";
+        _roadList.AddLast(road);
+        return road.GetComponent<BoxCollider>().bounds.size.z;
+    }
+
+    private static string CleanPath(string path) {
+        path = new Uri(path).AbsolutePath;
+        var start = path.IndexOf(@"RoadPrefabs");
+        var end = path.IndexOf(@".prefab");
+        path = path.Substring(start, end - start);
+        return Uri.UnescapeDataString(path);
+    }
+}
