24439fd176d49d74b40106ec85dc6992a6170cfd
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-04-09 15:37:21 +0200

    Window shopper prototype Cleaned up some script structure

---
+++ 20-min-exp/Assets/Scripts/WindowShopping/Buyable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buyable : Selectable {
+
+    // Use this for initialization
+    private void Start() {}
+
+    // Update is called once per frame
+    private void Update() {}
+
+    public override void Select() {
+        Destroy(gameObject);
+    }
+
+}
