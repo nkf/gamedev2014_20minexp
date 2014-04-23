661f7f2b2de3e029d98f94cb868ee57a136acfbd
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-04-23 20:30:12 +0200

    Util

---
+++ 20-min-exp/Assets/Scripts/HouseSale/Sellable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sellable : Selectable {
+
+    public int SellPrice = 500;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public override void Select() {
+        Toolbox.Instance.gameState.MoneyCounter += SellPrice;
+        Destroy(gameObject);
+    }
+}
---
+++ 20-min-exp/Assets/Scripts/Util/ResourceUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class ResourceUtil {
+
+    public static string[] GetPrefabPaths(string folderName) {
+        var path = Application.dataPath + "/Resources/" + folderName;
+	    return Directory.GetFiles(path).Select(s => CleanPath(s, folderName)).Distinct().ToArray();
+    }
+
+    private static string CleanPath(string path, string folderName) {
+        path = new Uri(path).AbsolutePath;
+        var start = path.IndexOf(folderName);
+        var end = path.IndexOf(@".prefab");
+        path = path.Substring(start, end - start);
+        return Uri.UnescapeDataString(path);
+    }
+}
