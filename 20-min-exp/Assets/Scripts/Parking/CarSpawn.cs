0f3a320af9d7117166dc8e8f38f4d04cbf603cc9
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-04-24 20:04:32 +0200

    Random Parking names + car spawns

---
+++ 20-min-exp/Assets/Scripts/Parking/CarSpawn.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using System.Collections;
+
+public class CarSpawn : MonoBehaviour {
+
+    private static readonly List<CarSpawn> All = new List<CarSpawn>();
+    void Awake() {
+        All.Add(this);
+    }
+    private enum Pattern {
+        Free, Taken, Main
+    }
+    private static Pattern[] _spawnPattern; 
+    private static void CreateSpawnPattern() {
+        if (_spawnPattern != null) return;
+        _spawnPattern = new Pattern[All.Count];
+        var cap = (int)Mathf.Floor(ParkingConfiguration.SpawnPct*All.Count);
+        for (int i = 0; i < All.Count; i++) {
+            if(i < cap) _spawnPattern[i] = Pattern.Taken;
+            if(i == cap) _spawnPattern[i] = Pattern.Main;
+            if(i > cap) _spawnPattern[i] = Pattern.Free;
+        }
+        _spawnPattern.Shuffle();
+    }
+
+    private static int _index;
+    // Use this for initialization
+	void Start () {
+        if(_carPaths == null) _carPaths = ResourceUtil.GetPrefabPaths("CarPrefabs");
+	    CreateSpawnPattern();
+	    var pattern = _spawnPattern[_index++];
+	    if (pattern == Pattern.Main) {
+	        var name = Toolbox.Instance.gameState.DayCounter == 2 ? "" : ParkingConfiguration.MainName;
+	        GetComponentInChildren<TextMesh>().text = name;
+	    } else {
+	        if(pattern == Pattern.Taken) SpawnRandomCar();
+	        GetComponentInChildren<TextMesh>().text = ParkingConfiguration.GetRandomName();
+	        GetComponent<BoxCollider>().enabled = false;
+	    }
+	}
+    private readonly System.Random _rng = new System.Random();
+    private static string[] _carPaths;
+    private void SpawnRandomCar() {
+        var load = Resources.Load<GameObject>(_carPaths[_rng.Next(0, _carPaths.Length)]);
+        var car = (GameObject)Instantiate(load);
+        var pos = transform.position;
+        pos.y +=  0.7f;
+        car.transform.position = pos;
+        car.transform.parent = transform;
+        car.transform.localEulerAngles += transform.localEulerAngles;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 20-min-exp/Assets/Scripts/Parking/ParkingConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParkingConfiguration : MonoBehaviour {
+
+    public int SpawnPercentage = 80;
+    public static float SpawnPct;
+    public string MainCharactersName;
+    public string[] ParkingNames;
+    private static string[] _names;
+    public static string MainName;
+    void Awake() {
+        SpawnPct = (SpawnPercentage/100f);
+        MainName = MainCharactersName;
+        _names = ParkingNames;
+        _names.Shuffle();
+    }
+
+    private static int _index;
+    public static string GetRandomName() {
+        if (_index <= _names.Length) return _names[_index++];
+        Debug.Log("OUT OF RANDOM NAMES :("); 
+        return "";
+    }
+    
+}
