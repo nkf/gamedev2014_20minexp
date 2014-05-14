efa06821b70da3a9f21480896fdb7c9446b3c13f
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-05-14 12:29:42 +0200

    Name tag in office scene

---
+++ 20-min-exp/Assets/Scripts/Office/GetName.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetName : MonoBehaviour {
+
+    private TextMesh _tag;
+	void Start () {
+	    _tag = GetComponent<TextMesh>();
+	    _tag.text = Toolbox.Instance.gameState.CharacterName;
+	}
+}
